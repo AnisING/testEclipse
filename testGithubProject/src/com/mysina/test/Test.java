25e6551ac60e27804a930c0725c5e0d74537eb1a
anis <anis_saied@hotmail.com>
2017-11-19 15:22:18 +0100

    add new line in test.java file

--- testGithubProject/src/com/mysina/test/Test.java
+++ testGithubProject/src/com/mysina/test/Test.java
@@ -1,12 +1,10 @@
 package com.mysina.test;
 
 public class Test {
-
 	public static void main(String[] args) {
 		System.out.println("I'm testing my project");
 		System.out.println("First changes to commit it!");
-		
-
+		System.out.println("add new line !");
 	}
 
 }
